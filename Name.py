892d7b823177a2ebf293e08ede3fe5edae369db0
Caston Hilcher <castonhilcher@gmail.com>
2020-02-19 08:45:47 -0600

    add: first commit with a name function

---
+++ Name.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name
+
+if __name__ == '__main__':
+
+    print('Hello world!')
+    print('What is your name?')
+    myName = input()
+    print('Nice to meet you, ' + myName)
+    print('How old are you?')
+    myAge = input()
+
