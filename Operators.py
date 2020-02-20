44d60620e21cc1189b861bd8f633b01e759cd894
Caston Hilcher <castonhilcher@gmail.com>
2020-02-20 08:04:01 -0600

    add: math operations

---
+++ Operators.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    3 ** 3  # 9, exponent
+    5 % 2  # 1, modulas
+    5 // 2  # 2, integer division
+    5 / 2  # 2.5, division
+    3 * 2  # 6, multiplication
+    5 - 3  # 2, subtraction
+    5 + 3  # 8, addition
